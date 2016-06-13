b7f597b677574f5daf8899ed42b37d6420544e42
emerlin <emerlin@newforma.com>
2016-06-13 16:53:11 -0400

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoOrganizr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoseSourceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            tbSource.Text = dialog.SelectedPath;
+            AddToLog($"Nouveau dossier source selectioné: {tbSource.Text}");
+            int filesToBeTreated = Organizer.CountNumberOfJpgFiles(tbSource.Text);
+            AddToLog($"Nombre de photos à trier: {filesToBeTreated}");
+        }
+
+        private void ChoseDestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            tbDest.Text = dialog.SelectedPath;
+        }
+
+        private void AddToLog(string log)
+        {
+            tblog.AppendText(log + Environment.NewLine);
+            tblog.ScrollToEnd();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbSource.Text)) { return; }
+            if (string.IsNullOrWhiteSpace(tbDest.Text)) { return; }
+
+            AddToLog(Organizer.ReadExif(tbSource.Text));
+        }
+    }
+}
---
+++ Organizer.cs
@@ -0,0 +1,47 @@
+﻿using ExifLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoOrganizr
+{
+   public class Organizer
+    {
+        public static int CountNumberOfJpgFiles(string path) {
+
+            return GetFiles(path).Count();
+        }
+
+        private static string[] GetFiles(string path)
+        {
+            return Directory.GetFiles(path).Where(x => x.ToLower().EndsWith(".jpg")).ToArray();
+        }
+
+
+        public static string  ReadExif(string path) {
+
+            string[] files = GetFiles(path);
+            StringBuilder sb = new StringBuilder();
+            foreach(string file in files)
+            {
+                try
+                {
+                    using (ExifReader reader = new ExifReader(file))
+                    {
+                        DateTime dateTaken = DateTime.MinValue;
+                        reader.GetTagValue(ExifTags.DateTimeOriginal, out dateTaken);
+                        sb.AppendLine($"{file} was taken on: {dateTaken}");
+                    }
+                }
+                catch (Exception)
+                {
+                    sb.AppendLine($"«Could not read exif for : {file}");
+                }
+                         }
+            return sb.ToString();
+        }
+    }
+}
